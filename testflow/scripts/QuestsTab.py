2995d2d1d6b663da8e49d78408193d8882a8e68b
axemaneric <ericfan1999@hotmail.com>
2018-08-31 16:05:00 -0700

    Add documentation

--- testflow/scripts/QuestsTab.py
+++ testflow/scripts/QuestsTab.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 __author__ = "Eric"
 
+# Test script for Quests function in top bar of Quirk.
 
 import time
 
@@ -10,8 +11,20 @@ from poco.exceptions import PocoNoSuchNodeException
 from airtest.core.api import *
 
 
+#--------------------------------------------------------------------#
+#   TESTFLOW                                                         #
+#--------------------------------------------------------------------#
+
+# BEGIN: Quests page -> claimRewardsTest-> swipeRightTest -> replaceQuestsTest
+# -> END
+# Refer to individual methods for individual test breakdown
+
+#--------------------------------------------------------------------#
+#   TESTCASE                                                         #
+#--------------------------------------------------------------------#
 class QuestsTab(QuirkCase):
 
+    # install Quirk and generate a default avatar
     @classmethod
     def setUpClass(cls):
         super(QuestsTab, cls).setUpClass()
@@ -21,6 +34,7 @@ class QuestsTab(QuirkCase):
     def setUp(self):
         self.poco("daily_quests").click()
 
+    # check if initial state is Quest page then run tests in order
     def runTest(self):
         # snapshot('../../res/img/quests.png')
         self.assertTrue(
@@ -30,6 +44,7 @@ class QuestsTab(QuirkCase):
         self.replaceQuestsTest()
 
     # to test if quests page has more quests on the right
+    # by swipe right and assert change
     def swipeRightTest(self):
         viewport = self.poco("viewport")
         viewport.focus([0.8, 0.5]).drag_to(viewport.focus([0.2, 0.5]))
@@ -38,6 +53,10 @@ class QuestsTab(QuirkCase):
             exists(Template(self.R('res/img/queststab/after_swipe.png'))), "Swipe right failed")
 
     # Tests if Avatar shop reward can be claimed
+    # BEGIN: Quests page -> Exit to Commons -> Access Avatar through MenuBar ->
+    # Enter Avatar Saloon -> Exit to Commons -> Open Quests page -> Check if
+    # Avatar Visit quest is claimable -> Claim rewards -> Check if rewards
+    # have been recieved -> END
     def claimRewardsTest(self):
 
         self.poco("exit_container").click()
@@ -63,6 +82,10 @@ class QuestsTab(QuirkCase):
         self.assertTrue(exists(
             Template(self.R('res/img/queststab/150.png'))), "Gold value did not change")
 
+    # Test for checking if replacing quests work
+    # BEGIN: Quests page -> Find nearest Dismiss button -> Replace quest ->
+    # Repeat until no more Dismiss button exists -> Check if number of quests
+    # replaced equals allowed amount (default is 3)
     def replaceQuestsTest(self, dismisses_allowed=3):
 
         quests_dismissed = 0
