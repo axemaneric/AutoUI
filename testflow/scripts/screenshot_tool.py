02cb5d41d71737cf64569fc36f6223ddb7509a36
axemaneric <ericfan1999@hotmail.com>
2018-09-07 17:39:14 -0700

    add tokens script

--- testflow/scripts/screenshot_tool.py
+++ testflow/scripts/screenshot_tool.py
@@ -4,5 +4,5 @@ from poco.drivers.unity3d import UnityPoco
 from airtest.core.api import snapshot
 
 poco = UnityPoco()
-path = snapshot('../../res/img/Commons.jpg')
+path = snapshot('../../res/img/tokens/machine2.jpg')
 print("path:" + path)
